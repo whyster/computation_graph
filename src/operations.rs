8752017ec3ec91e6443f99dd022cd7cd3de0398a
Aleksey Kalinovski <aleksey.kalinovski@gmail.com>
2025-07-14 19:29:25 -0400

    fix: add support for operations to calculate the result

--- src/operations.rs
+++ src/operations.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, BitAnd, BitOr, BitXor, Div, Mul, Neg, Not, Shl, Shr, Sub};
+use std::process::Output;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum BinaryOperation { 
@@ -12,6 +14,32 @@ pub enum BinaryOperation {
     BitwiseXor,
 }
 
+impl BinaryOperation {
+    pub fn perform<L, R, O>(&self, left: L, right: R) -> O
+    where L: Add<R, Output = O>
+           + Sub<R, Output = O>
+           + Div<R, Output = O>
+           + Mul<R, Output = O>
+           + BitAnd<R, Output = O>
+           + BitOr<R, Output = O>
+           + BitXor<R, Output = O>
+           + Shl<R, Output = O>
+           + Shr<R, Output = O> {
+        match self {
+            BinaryOperation::Add => left + right,
+            BinaryOperation::Subtract => left - right,
+            BinaryOperation::Divide => left / right,
+            BinaryOperation::Multiply => left * right,
+            BinaryOperation::BitwiseAnd => left & right,
+            BinaryOperation::BitwiseLeftShift => left << right,
+            BinaryOperation::BitwiseRightShift => left >> right,
+            BinaryOperation::BitwiseOr => left | right,
+            BinaryOperation::BitwiseXor => left ^ right,
+        }
+    }
+}
+
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum BooleanOperation {
     And,
@@ -22,8 +50,19 @@ pub enum BooleanOperation {
 pub enum UnaryOperation {
     Not,
     Invert,
-    UnaryPlus,
     UnaryMinus,
+}
+
+impl UnaryOperation {
+    pub fn perform<T, R>(&self, operand: T) -> R
+    where T: Not<Output = R>
+           + Neg<Output = R>{
+        match self {
+            UnaryOperation::Not => !operand,
+            UnaryOperation::Invert => !operand,
+            UnaryOperation::UnaryMinus => -operand,
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
